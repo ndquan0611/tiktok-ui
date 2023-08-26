598266ceb11f8f895f790338f862fa15a543094d
dinhquan0611 <nguyendinhquan78910@gmail.com>
2023-08-26 17:07:17 +0700

    Dựng khung Layout mặc định: npm i classnames

--- src/components/Layouts/HeaderOnly/index.js
+++ src/components/Layouts/HeaderOnly/index.js
@@ -1,17 +1,15 @@
-import Header from '~/components/Layout/components/Header';
-import Sidebar from './Sidebar';
+import Header from '~/components/Layouts/components/Header';
 
-function DefaultLayout({ children }) {
+function HeaderOnly({ children }) {
     console.log(children);
     return (
         <div>
             <Header />
             <div className="container">
-                <Sidebar />
                 <div className="content">{children}</div>
             </div>
         </div>
     );
 }
 
-export default DefaultLayout;
+export default HeaderOnly;
